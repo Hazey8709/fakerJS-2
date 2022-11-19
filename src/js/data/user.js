69355b50d70ab428426b66bcfce06b85c00a1743
Hazey8709 <jfortin802@gmail.com>
2022-11-19 16:21:11 -0500

    commit

--- src/js/data/user.js
+++ src/js/data/user.js
@@ -1,5 +1,19 @@
 import { faker } from "@faker-js/faker";
 
+const avatar = faker.image.avatar();
 const name = faker.name.firstName();
+const email = faker.internet.email();
+const userId = faker.datatype.uuid();
+const birthdate = faker.date.birthdate();
+const vehicle = faker.vehicle.vehicle();
 
-export default name;
+const userProfile = {
+    avatar,
+    name,
+    email,
+    userId,
+    birthdate,
+    vehicle,
+};
+
+export default userProfile;
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
-import name from "./data/user";
+import userProfile from "./data/user";
+
+const UserSection = document.getElementById("user");
+const UserList = document.createElement("ul");
+
+const buildUser = (userData) => {
+    for (const prop in userData) {
+        const lineItemContent = `${prop} : ${userData[prop]} `;
+        const lineItem = document.createElement("li");
+
+        lineItem.innerText = lineItemContent;
+        UserList.appendChild(lineItem);
+        UserSection.appendChild(UserList);
+    }
+};
 
 (() => {
     console.log("working!");
-    console.log(name);
+    console.log(userProfile);
+    buildUser(userProfile);
 })();
